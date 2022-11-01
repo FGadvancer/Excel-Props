a9706a06ef65e03c864e9175e0a8dc3c04753a7e
Gordon <1432970085@qq.com>
2022-11-01 15:13:41 +0800

    docker compose update

--- cmd/template/main.go
+++ cmd/template/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"Excel-Props/pkg/db"
 	"Excel-Props/pkg/log"
-	"Excel-Props/pkg/utils"
 	"fmt"
 	"github.com/tealeg/xlsx"
 	"os"
@@ -54,18 +53,20 @@ func main() {
 		temp := new(db.Template2)
 		if i > 0 {
 			if f.Sheets[page].Cell(i, 0).Value != "" && f.Sheets[page].Cell(i, 4).Value != "" {
-				temp.MaterialKey = f.Sheets[page].Cell(i, 0).Value
-				temp.MaterialCategory = f.Sheets[page].Cell(i, 1).Value
-				temp.MaterialName = f.Sheets[page].Cell(i, 2).Value
-				temp.MaterialSubstance = f.Sheets[page].Cell(i, 3).Value
-				temp.MaterialStandard = f.Sheets[page].Cell(i, 4).Value
-				temp.Quantity = utils.StringToInt32(f.Sheets[page].Cell(i, 5).Value)
-				temp.MaterialUnit = f.Sheets[page].Cell(i, 6).Value
-				temp.ProcessingCategory = f.Sheets[page].Cell(i, 7).Value
-				temp.RemarkOne = f.Sheets[page].Cell(i, 8).Value
-				temp.RemarkTwo = f.Sheets[page].Cell(i, 9).Value
-				temp.IsPurchase = f.Sheets[page].Cell(i, 10).Value
-				temp.StandardCraft = f.Sheets[page].Cell(i, 11).Value
+				//temp.MaterialKey = f.Sheets[page].Cell(i, 0).Value
+				//
+				//temp.MaterialCategory = f.Sheets[page].Cell(i, 1).Value
+				//temp.MaterialName = f.Sheets[page].Cell(i, 2).Value
+				//temp.MaterialSubstance = f.Sheets[page].Cell(i, 3).Value
+				//temp.MaterialStandard = f.Sheets[page].Cell(i, 4).Value
+				//temp.Quantity = utils.StringToInt32(f.Sheets[page].Cell(i, 5).Value)
+				//temp.MaterialUnit = f.Sheets[page].Cell(i, 6).Value
+				//temp.ProcessingCategory = f.Sheets[page].Cell(i, 7).Value
+				//temp.RemarkOne = f.Sheets[page].Cell(i, 8).Value
+				//temp.RemarkTwo = f.Sheets[page].Cell(i, 9).Value
+				//temp.IsPurchase = f.Sheets[page].Cell(i, 10).Value
+				//temp.StandardCraft = f.Sheets[page].Cell(i, 11).Value
+				fmt.Println(f.Sheets[page].Cell(i, 0).Value)
 			}
 		}
 		list2 = append(list2, temp)
