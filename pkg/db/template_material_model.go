3c58ba69cd00e4a6705945d87be39b2cf18e7511
Gordon <1432970085@qq.com>
2022-11-24 15:32:12 +0800

    file update

--- pkg/db/template_material_model.go
+++ pkg/db/template_material_model.go
@@ -52,7 +52,7 @@ func (t *TemplateMaterial) DeleteAllTemplateMaterial() error {
 }
 func (t *TemplateMaterial) GetAllMaterialTemplates() ([]*TemplateMaterial, error) {
 	var templateList []TemplateMaterial
-	err := utils.Wrap(DB.MysqlDB.db.Find(&templateList).Order("index ASC").Error,
+	err := utils.Wrap(DB.MysqlDB.db.Debug().Find(&templateList).Order("index ASC").Error,
 		"GetAllMaterialTemplates failed")
 	var transfer []*TemplateMaterial
 	for _, v := range templateList {
